abcfc80d51595c1947a6c9650c776afee4ca370c
antonio9916 <antonio-98990@gmail.com>
2024-06-28 19:50:50 -0400

    Add CRUD functionality for tasks and restrict access to authenticated users

--- users/views.py
+++ users/views.py
@@ -6,7 +6,43 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Task
 from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
+@method_decorator(login_required, name='dispatch')
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+
+@method_decorator(login_required, name='dispatch')
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+
+@method_decorator(login_required, name='dispatch')
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = 'task_form.html'
+    fields = ['title', 'description', 'completed', 'user']  # Asegúrate de incluir el campo 'user'
+    success_url = reverse_lazy('task_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+@method_decorator(login_required, name='dispatch')
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'task_form.html'
+    fields = ['title', 'description', 'completed', 'user']  # Asegúrate de incluir el campo 'user'
+    success_url = reverse_lazy('task_list')
+
+@method_decorator(login_required, name='dispatch')
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'task_confirm_delete.html'
+    success_url = reverse_lazy('task_list')
 
 class TaskListView(ListView):
     model = Task
